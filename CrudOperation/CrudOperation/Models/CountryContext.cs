5cc7c059beb4fb6bc4d610dc0f850cedc3a5f2c5
D4678 <dhuruvmourya@gmail.com>
2024-09-30 20:17:28 -0700

    first commit

---
+++ CrudOperation/CrudOperation/Models/CountryContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CrudOperation.Models
+{
+    public partial class CountryContext : DbContext
+    {
+        public CountryContext()
+        {
+        }
+
+        public CountryContext(DbContextOptions<CountryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-323JE90;Database=Country;Trusted_Connection=True;Trust Server Certificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.ToTable("country");
+
+                entity.Property(e => e.Capital)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Currency)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Economy).HasColumnType("numeric(18, 0)");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
